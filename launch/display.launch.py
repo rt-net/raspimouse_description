2cfbb155a22ab82866d49904bb27e27a12fe1111
YusukeKato <yusuke.kato@rt-net.jp>
2025-07-24 16:53:20 +0900

    Nodeの引数の順序を統一。Ruffによる自動整形を実施。

--- launch/display.launch.py
+++ launch/display.launch.py
@@ -24,29 +24,25 @@ from raspimouse_description.robot_description_loader import RobotDescriptionLoad
 
 def generate_launch_description():
     declare_arg_lidar = DeclareLaunchArgument(
-        'lidar',
-        default_value='none',
-        description='Set "none", "urg", "lds", or "rplidar".')
+        'lidar', default_value='none', description='Set "none", "urg", "lds", or "rplidar".'
+    )
     declare_arg_lidar_frame = DeclareLaunchArgument(
-        'lidar_frame',
-        default_value='laser',
-        description='Set lidar link name.')
+        'lidar_frame', default_value='laser', description='Set lidar link name.'
+    )
     declare_arg_namespace = DeclareLaunchArgument(
-        'namespace',
-        default_value='',
-        description='Set namespace for tf tree.')
+        'namespace', default_value='', description='Set namespace for tf tree.'
+    )
     declare_arg_use_rviz = DeclareLaunchArgument(
-        'use_rviz',
-        default_value='true',
-        description='Set "true" to launch rviz.')
+        'use_rviz', default_value='true', description='Set "true" to launch rviz.'
+    )
     declare_arg_use_rgb_camera = DeclareLaunchArgument(
-        'use_rgb_camera',
-        default_value='false',
-        description='Set "true" to mount rgb camera.')
+        'use_rgb_camera', default_value='false', description='Set "true" to mount rgb camera.'
+    )
     declare_arg_camera_downward = DeclareLaunchArgument(
         'camera_downward',
         default_value='false',
-        description='Set "true" to point the camera downwards.')
+        description='Set "true" to point the camera downwards.',
+    )
 
     description_loader = RobotDescriptionLoader()
     description_loader.lidar = LaunchConfiguration('lidar')
@@ -56,35 +52,43 @@ def generate_launch_description():
 
     push_ns = PushRosNamespace([LaunchConfiguration('namespace')])
 
-    rsp = Node(package='robot_state_publisher',
-               executable='robot_state_publisher',
-               output='both',
-               parameters=[{'robot_description': description_loader.load()}])
+    rsp = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='both',
+        parameters=[{'robot_description': description_loader.load()}],
+    )
 
     jsp = Node(
         package='joint_state_publisher_gui',
         executable='joint_state_publisher_gui',
         output='screen',
-        condition=IfCondition(LaunchConfiguration('use_rviz')))
+        condition=IfCondition(LaunchConfiguration('use_rviz')),
+    )
 
-    rviz_config_file = get_package_share_directory(
-        'raspimouse_description') + '/launch/config/urdf.rviz'
-    rviz_node = Node(package='rviz2',
-                     executable='rviz2',
-                     name='rviz2',
-                     output='log',
-                     arguments=['-d', rviz_config_file],
-                     condition=IfCondition(LaunchConfiguration('use_rviz')))
+    rviz_config_file = (
+        get_package_share_directory('raspimouse_description') + '/launch/config/urdf.rviz'
+    )
+    rviz_node = Node(
+        name='rviz2',
+        package='rviz2',
+        executable='rviz2',
+        output='log',
+        arguments=['-d', rviz_config_file],
+        condition=IfCondition(LaunchConfiguration('use_rviz')),
+    )
 
-    return LaunchDescription([
-        declare_arg_lidar,
-        declare_arg_lidar_frame,
-        declare_arg_namespace,
-        declare_arg_use_rviz,
-        declare_arg_use_rgb_camera,
-        declare_arg_camera_downward,
-        push_ns,
-        rsp,
-        jsp,
-        rviz_node,
-        ])
+    return LaunchDescription(
+        [
+            declare_arg_lidar,
+            declare_arg_lidar_frame,
+            declare_arg_namespace,
+            declare_arg_use_rviz,
+            declare_arg_use_rgb_camera,
+            declare_arg_camera_downward,
+            push_ns,
+            rsp,
+            jsp,
+            rviz_node,
+        ]
+    )
